70e642ea5019c60022c433282fafddde515d7f02
jadon-lotsman <kirill.gorev1@mail.ru>
2026-08-30 21:56:41 +0700

    add clearSelection method

--- Mnemo.Vue/src/shared/composables/useSelection.ts
+++ Mnemo.Vue/src/shared/composables/useSelection.ts
@@ -1,5 +1,5 @@
 import { useEventListener } from '@vueuse/core'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 export function useSelection() {
   const hasSelection = ref<boolean>(false)
@@ -11,12 +11,24 @@ export function useSelection() {
     hasSelection.value = text.length > 0
   }
 
-  onMounted(() => updateSelection())
+  function clearSelection() {
+    const selection = window.getSelection()
+    if (selection) {
+      selection.removeAllRanges()
+    }
+
+    hasSelection.value = false
+    selectedText.value = ''
+  }
+
   useEventListener('selectionchange', updateSelection)
   useEventListener('mouseup', updateSelection)
+
+  updateSelection()
 
   return {
     hasSelection,
     selectedText,
+    clearSelection,
   }
 }
