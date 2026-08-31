ae9866c2667e9de6a42608d98b2560b5d859de54
jadon-lotsman <kirill.gorev1@mail.ru>
2026-08-31 20:18:14 +0700

    add visibility property

--- Mnemo.Api/Data/Entities/VocabularyPack.cs
+++ Mnemo.Api/Data/Entities/VocabularyPack.cs
@@ -1,10 +1,15 @@
-﻿namespace Mnemo.Data.Entities
+﻿using Mnemo.Shared.Enums;
+
+namespace Mnemo.Data.Entities
 {
     public class VocabularyPack
     {
         public int Id { get; set; }
+        public Guid Guid { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Visibility Visibility { get; set; }
+        public int ImportCount { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
@@ -14,12 +19,20 @@
         public List<VocabularyPackEntry> PackEntries { get; set; }
 
 
-
         public VocabularyPack()
         {
-            PackEntries = new List<VocabularyPackEntry>();
+            Visibility = Visibility.Private;
             CreatedAt = DateTime.UtcNow;
             UpdatedAt = CreatedAt;
+
+            Guid = Guid.NewGuid();
+            PackEntries = new List<VocabularyPackEntry>();
+        }
+
+
+        public void RevokeGuid()
+        {
+            Guid = Guid.NewGuid();
         }
     }
 }
